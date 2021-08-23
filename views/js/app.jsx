7b4aea147a7c4225f9e3fe39bc66d159948adf2b
juandreww <juandreww@outlook.com>
2021-08-23 22:55:25 +0700

    app.jsx()

--- views/js/app.jsx
+++ views/js/app.jsx
@@ -11,14 +11,14 @@ class App extends React.Component {
 class Home extends React.Component {
   render() {
     return (
-    <div className="container">
-        <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
-        <h1>Jokeish</h1>
-        <p>A load of Dad jokes XD</p>
-        <p>Sign in to get access </p>
-        <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
-        </div>
-    </div>
+      <div className = "container">
+          <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
+          <h1>Jokeish</h1>
+          <p>A load of Dad jokes XD</p>
+          <p>Sign in to get access </p>
+          <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+          </div>
+      </div>
     )
   }
 }
@@ -50,37 +50,37 @@ class LoggedIn extends React.Component {
     }
   }
 
-  class Joke extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        liked: ""
-      }
-      this.like = this.like.bind(this);
+class Joke extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      liked: ""
     }
-      
-    like() {
-      // ... we'll add this block later
-    }
-      
-    render() {
-      return (
-        <div className="col-xs-4">
-          <div className="panel panel-default">
-            <div className="panel-heading">#{this.props.joke.id} <span className="pull-right">{this.state.liked}</span></div>
-            <div className="panel-body">
-              {this.props.joke.joke}
-            </div>
-            <div className="panel-footer">
-              {this.props.joke.likes} Likes &nbsp;
-              <a onClick={this.like} className="btn btn-default">
-                <span className="glyphicon glyphicon-thumbs-up"></span>
-              </a>
-            </div>
+    this.like = this.like.bind(this);
+  }
+    
+  like() {
+    // ... we'll add this block later
+  }
+    
+  render() {
+    return (
+      <div className="col-xs-4">
+        <div className="panel panel-default">
+          <div className="panel-heading">#{this.props.joke.id} <span className="pull-right">{this.state.liked}</span></div>
+          <div className="panel-body">
+            {this.props.joke.joke}
+          </div>
+          <div className="panel-footer">
+            {this.props.joke.likes} Likes &nbsp;
+            <a onClick={this.like} className="btn btn-default">
+              <span className="glyphicon glyphicon-thumbs-up"></span>
+            </a>
           </div>
         </div>
-      )
-    }
+      </div>
+    )
   }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
